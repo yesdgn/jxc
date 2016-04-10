45bfe8a1850fcb6e8631343b4357e38b7bfb9bec
yesdgn <yesdgn@qq.com>
2016-04-10 21:49:09 +0800

    1

---
+++ src/component/App.jsx
@@ -0,0 +1,24 @@
+
+'use strict';
+import  React  from 'react';
+import { DatePicker } from 'antd';
+class App extends React.Component {
+    static defaultProps = {
+    };
+    static propTypes = {
+    };
+    constructor(props) {
+        super(props);
+        this.state={
+        };
+    };
+    componentWillMount() {
+    };
+    render() {
+        return(
+          <DatePicker />
+        );
+    }
+};
+
+export default App
